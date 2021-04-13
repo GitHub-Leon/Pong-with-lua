46b46f07d664553c2dfb0485e90d8368e91cf225
GitHub-Leon <leon.pliessnig@outlook.com>
2021-04-13 10:16:52 +0200

    Update main.lua
    added player collision

--- main.lua
+++ main.lua
@@ -4,7 +4,9 @@ local WINDOW_HEIGHT = 720
 
 -- Rectangle
 RECTANGLE_SPEED = 400
-math.randomseed(os.time)
+RECTANGLE_WIDTH = 20
+RECTANGLE_HEIGHT = 100
+math.randomseed(os.time())
 
 
 -- Main Functions
@@ -20,17 +22,20 @@ function love.load()
 
   -- init players
   player_one = {}
-  player_one.y = 30
+  player_one.y = 10
+  player_one.x = 10
   player_one.score = 0
 
   player_two = {}
-  player_two.y = WINDOW_HEIGHT-130
+  player_two.y = WINDOW_HEIGHT-110
+  player_two.x = WINDOW_WIDTH-30
   player_two.score = 0
 
   -- init ball
   ball = {}
   ball.x = WINDOW_WIDTH/2-7
   ball.y = WINDOW_HEIGHT/2-7
+  ball.radius = 15
 
   ball.dx = math.random(2) == 1 and 300 or -300
   ball.dy = math.random(-50, 50)
@@ -54,9 +59,51 @@ function love.update(dt)
     player_two.y = math.min(WINDOW_HEIGHT-110, player_two.y + RECTANGLE_SPEED * dt)
   end
 
+
   -- update ball movement
   ball.x = ball.x + ball.dx * dt
   ball.y = ball.y + ball.dy * dt
+
+
+  -- detect collision player one
+  collision = true
+  if ball.x > player_one.x + RECTANGLE_WIDTH or player_one.x > ball.x + ball.radius then 
+    collision = false
+  end
+  if ball.y > player_one.y + RECTANGLE_HEIGHT or player_one.y > ball.y + ball.radius then
+    collision = false
+  end
+
+  if collision then
+    ball.dx = -ball.dx * 1.1
+    ball.x = player_one.x + 15
+
+    if ball.dy < 0 then
+      ball.dy = -math.random(10,150)
+    else
+      ball.dy = math.random(10, 150)
+    end
+  end
+
+    -- detect collision player two
+    collision = true
+    if ball.x > player_two.x + RECTANGLE_WIDTH or player_two.x > ball.x + ball.radius then 
+      collision = false
+    end
+    if ball.y > player_two.y + RECTANGLE_HEIGHT or player_two.y > ball.y + ball.radius then
+      collision = false
+    end
+  
+    if collision then
+      ball.dx = -ball.dx * 1.1
+      ball.x = player_two.x - 15
+  
+      if ball.dy < 0 then
+        ball.dy = -math.random(10,150)
+      else
+        ball.dy = math.random(10, 150)
+      end
+    end
 end
 
 
@@ -67,7 +114,7 @@ function love.draw()
 
 
     -- Update positions
-    love.graphics.rectangle('fill', 10, player_one.y, 20, 100)
-    love.graphics.rectangle('fill', WINDOW_WIDTH-30, player_two.y, 20, 100)
-    love.graphics.rectangle('fill', ball.x, ball.y, 15, 15)
+    love.graphics.rectangle('fill', player_one.x, player_one.y, RECTANGLE_WIDTH, RECTANGLE_HEIGHT)
+    love.graphics.rectangle('fill', player_two.x, player_two.y, RECTANGLE_WIDTH, RECTANGLE_HEIGHT)
+    love.graphics.rectangle('fill', ball.x, ball.y, ball.radius, ball.radius)
 end
